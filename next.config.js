63eb3f326d1829e8d7b609c8ed448371649a9750
gabriel.cardoso1.ext <gabriel.cardoso1.ext@bayer.com>
2023-07-14 18:03:58 -0300

    feat: Allow LAN hosting the app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+IS_DEV_ENV = process.env.NODE_ENV === 'development';
+ALLOW_LAN_ROUTING = false
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+
+if (IS_DEV_ENV) {
+    if (ALLOW_LAN_ROUTING) {
+        console.log('info  - lanUrl:', `http://${require('address').ip()}:3000`)
+    }
+}
 
 module.exports = nextConfig
