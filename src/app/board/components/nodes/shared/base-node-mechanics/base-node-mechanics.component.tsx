3417f398cd697b029f36cac5b95280ce0fcf8a76
gabriel.cardoso1.ext <gabriel.cardoso1.ext@bayer.com>
2023-07-23 21:48:25 -0300

    feat: Add Base Node Mechanics

---
+++ src/app/board/components/nodes/shared/base-node-mechanics/base-node-mechanics.component.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { NodeProps, Handle, Position, HandleProps } from "reactflow";
+import NodeResizer from "../node-resizer/node-resizer.component";
+import styles from "./base-node-mechanics.module.scss";
+
+export default function BaseNodeMechanics(props: NodeProps) {
+
+    const getHandleProps = (position: Position): HandleProps & { className: string } => {
+        const baseClassName = styles.handle;
+
+        switch (position) {
+            case Position.Top:
+                return {
+                    className: `${baseClassName} ${styles.handleTop}`,
+                    type: 'source',
+                    id: 'top',
+                    position
+                };
+
+            case Position.Right:
+                return {
+                    className: `${baseClassName} ${styles.handleRight}`,
+                    type: 'source',
+                    id: 'right',
+                    position
+                };
+
+            case Position.Bottom:
+                return {
+                    className: `${baseClassName} ${styles.handleBottom}`,
+                    type: 'source',
+                    id: 'bottom',
+                    position
+                };
+
+            case Position.Left:
+                return {
+                    className: `${baseClassName} ${styles.handleLeft}`,
+                    type: 'source',
+                    id: 'left',
+                    position
+                };
+        }
+    }
+
+    return (
+        <div className={styles.nodeWrapper}>
+            <NodeResizer
+                minHeight={100}
+                minWidth={100}
+                isVisible={props.selected}
+            />
+
+            <div className={styles.handleHoverContainer} />
+
+            <Handle { ...getHandleProps(Position.Top) } />
+            <Handle { ...getHandleProps(Position.Right) } />
+            <Handle { ...getHandleProps(Position.Bottom) } />
+            <Handle { ...getHandleProps(Position.Left) } />
+        </div>
+    )
+}
--- src/app/board/components/nodes/square-node/square-node.component.tsx
+++ src/app/board/components/nodes/square-node/square-node.component.tsx
@@ -1,22 +1,11 @@
-import { NodeProps, Handle, Position } from "reactflow";
-import NodeResizer from "../shared/node-resizer/node-resizer.component";
+import { NodeProps } from "reactflow";
+import BaseNodeMechanics from "../shared/base-node-mechanics/base-node-mechanics.component";
 import styles from "./square-node.module.scss";
 
-export default function SquareNode({
-    selected
-}: NodeProps) {
+export default function SquareNode(props: NodeProps) {
     return (
         <div className={styles.squareNode}>
-            <NodeResizer
-                minHeight={100}
-                minWidth={100}
-                isVisible={selected}
-            />
-
-            <Handle className={styles.handleTop} type="source" id="top" position={Position.Top} />
-            <Handle className={styles.handleRight} type="source" id="right" position={Position.Right} />
-            <Handle className={styles.handleBottom} type="source" id="bottom" position={Position.Bottom} />
-            <Handle className={styles.handleLeft} type="source" id="left" position={Position.Left} />
+            <BaseNodeMechanics {...props} />
         </div>
     )
 }
