74f8f1c8ca94fab21546c6b8a3c9d7bc93ca3a15
gabriel.cardoso1.ext <gabriel.cardoso1.ext@bayer.com>
2023-07-29 17:29:04 -0300

    feat: Add TextNode component

--- src/app/board/components/nodes/text-node/text-node.component.tsx
+++ src/app/board/components/nodes/text-node/text-node.component.tsx
@@ -1,15 +1,47 @@
+import { useEffect, useRef } from "react";
 import { NodeProps } from "reactflow";
 import BaseNodeMechanics from "../shared/base-node-mechanics/base-node-mechanics.component";
 import styles from "./text-node.module.scss";
-import { useRef } from "react";
+import { Manrope } from "next/font/google";
+
+const ManropeFont = Manrope({
+    weight: ['400', '500', '600', '700', '800'],
+    subsets: ['latin']
+});
 
 export default function TextNode(props: NodeProps) {
-    const ref = useRef<HTMLTextAreaElement>(null);
+    const textNodeRef = useRef<HTMLDivElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(textareaFocus, [])
+
+    function textareaFocus() {
+        if (textareaRef.current != null) textareaRef.current?.focus()
+    }
+
+    function adjustTextareaHeight() {
+        if (textNodeRef.current && textareaRef.current) {
+            if (textareaRef.current.scrollHeight >= textareaRef.current.clientHeight) {
+                textNodeRef.current.style.height = textareaRef.current.scrollHeight + 'px'
+            } else {
+                textareaRef.current.style.height = '30px';
+            }
+        }
+    }
 
     return (
-        <div className={styles.textNode} onDoubleClick={() => ref?.current?.focus()}>
+        <div
+            ref={textNodeRef}
+            className={styles.textNode}
+            onDoubleClick={textareaFocus}
+        >
             <BaseNodeMechanics {...props} />
-            <textarea ref={ref} autoFocus className={styles.textNodeTextarea} placeholder="T..." />
+            <textarea
+                ref={textareaRef}
+                className={`${styles.textNodeTextarea} ${ManropeFont.className}`}
+                placeholder="Start typing..."
+                onKeyDown={adjustTextareaHeight}
+            />
         </div>
     )
 }
